3cd0df7d5a7678177bdcc50359472c909c20060f
P.C <Phoebe@Konshin>
2025-09-27 17:03:56 +0200

    add agent

--- src/Effect_Execution_Details.h
+++ src/Effect_Execution_Details.h
@@ -1,17 +1,9 @@
 #pragma once
 
-
-
 struct Effect_Execution_Details {
-
 	char bytes1_38[38];
-
 	char bytes39_78[40];
-
 };
 
-
-
 void Read_Effect_Execution_Details(struct Effect_Execution_Details* details, FILE* read_file);
-
 void Write_Effect_Execution_Details(struct Effect_Execution_Details* details, FILE* write_file);
